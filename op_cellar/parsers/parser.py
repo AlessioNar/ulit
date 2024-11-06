38e6505953f27176636bb54e6d006ed04f3230a7
AlessioNar <alessio.nardin@gmail.com>
2024-11-06 17:01:12 +0100

    Created parser abstract class

--- op_cellar/parsers/parser.py
+++ op_cellar/parsers/parser.py
@@ -1,9 +1,5 @@
 from abc import ABC, abstractmethod
 
-from .formex import Formex4Parser
-from .akomantoso import AkomaNtosoParser
-from .html import HTMLParser
-
 class Parser(ABC):
     @abstractmethod
     def parse(self, data):
@@ -13,18 +9,3 @@ class XMLParser(Parser):
     def parse(self, data):
         # Your XML parsing logic here
         pass
-
-# Usage
-parsers = {
-    'html': HTMLParser(),
-    'xml': XMLParser(),
-    'formex4': Formex4Parser(),
-    'akoma_ntoso': AkomaNtosoParser(),
-}
-
-def parse_data(data, format, granularity='article'):
-    parser = parsers.get(format.lower())
-    if parser:
-        return parser.parse(data, granularity)
-    else:
-        raise ValueError('Unsupported format')
